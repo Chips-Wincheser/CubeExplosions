51981224ca3cd9770f9014f92872eec0293f8e71
Chips-Wincheser <gleba.trigub@mail.ru>
2024-12-27 14:16:03 +0700

    bug fixes

--- Assets/Script/CubeExplosions.cs
+++ Assets/Script/CubeExplosions.cs
@@ -1,85 +1,57 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CubeExplosions : MonoBehaviour
 {
     [SerializeField] private GameObject _cubePrefab;
+    [SerializeField] private CubeManager _cubeManager;
     [SerializeField] private float _explosionRadius;
-    [SerializeField] private float _explosionForce;
 
-    private int _minNumberRandomRange=2;
-    private int _maxNumberRandomRange=7;
+    private Camera _camera;
+    
+    private RaycastHit _hit;
+    private Ray _ray;
+
     private float _currentChance = 1.0f;
+    public float CurrentChance;
 
-    private float CurrentChance;
+    private void Awake()
+    {
+        Renderer _cubeRenderer = GetComponent<Renderer>();
+        _cubeRenderer.material.color = new Color(Random.value, Random.value, Random.value);
+    }
 
     private void Start()
     {
         if (CurrentChance<=0)
-        {
             CurrentChance = _currentChance;
-        }
+
+        _camera = Camera.main;
     }
 
-    private void OnMouseDown()
+    private void Update()
     {
-        Vector3 spawnPosition1 = transform.position;
-        Vector3 newCubScale = transform.localScale/2;
-        int numberDecreaseChance = 2;
-
-        if (Random.value<=CurrentChance)
+        if (Input.GetMouseButtonDown(0))
         {
-            for (int i = 0; i < Random.Range(_minNumberRandomRange, _maxNumberRandomRange); i++)
-            {
-                GameObject newCube = Instantiate(_cubePrefab, spawnPosition1, Quaternion.identity);
-                newCube.transform.localScale = newCubScale;
+            _ray = _camera.ScreenPointToRay(Input.mousePosition);
 
-                if (newCube.TryGetComponent(out CubeExplosions cubeScript))
+            if (Physics.Raycast(_ray, out _hit, Mathf.Infinity))
+            {
+                if(_hit.collider.gameObject == gameObject)
                 {
-                    cubeScript.CurrentChance = CurrentChance / numberDecreaseChance;
+                    Vector3 spawnPosition1 = transform.position;
+                    Vector3 newCubeScale = transform.localScale/2;
+
+                    if (Random.value<=CurrentChance)
+                    {
+                        _cubeManager.SpawnCubes(spawnPosition1, newCubeScale, CurrentChance, _cubePrefab);
+                        Destroy(gameObject);
+                    }
+                    else
+                    {
+                        Destroy(gameObject);
+                    }
                 }
-
-                Exlode();
-
-                PaintCube(newCube);
-            }
-             
-            Destroy(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    private void PaintCube(GameObject cube)
-    {
-        Renderer _cubeRenderer = cube.GetComponent<Renderer>();
-        _cubeRenderer.material.color = new Color(Random.value, Random.value, Random.value);
-    }
-
-    private void Exlode()
-    {
-        foreach (Rigidbody explodableObject in GetExplodableObjects())
-        {
-            explodableObject.AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
-        }
-    }
-
-    private List<Rigidbody> GetExplodableObjects()
-    {
-        Collider[] hits = Physics.OverlapSphere(transform.position, _explosionRadius);
-
-        List<Rigidbody> barrels = new ();
-
-        foreach (Collider hit in hits)
-        {
-            if(hit.attachedArticulationBody!= null)
-            {
-                barrels.Add(hit.attachedRigidbody);
             }
         }
-
-        return barrels;
     }
 }
